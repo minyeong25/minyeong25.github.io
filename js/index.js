a91d4b0fdb99784ac1ac2be830c4c27890b1c53f
minyeong25 <119823308+minyeong25@users.noreply.github.com>
2023-01-30 10:33:22 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-$(function () {});
+$(function () {
+  let nav = $("#nav").offset().top;
+
+  $(window).scroll(function () {
+    let window = $(this).scrollTop();
+    console.log(nav);
+    if (window > 0 && window < 900) {
+      $("header").addClass("fixed");
+    } else if (window >= 900) {
+      $(".skill_per").css({ "animation-play-state": "running" });
+    } else {
+      $("header").removeClass("fixed");
+    }
+    // else {
+    //   $(".skill_per").css({ "animation-play-state": "paused" });
+    // }
+  });
+
+  // $("#profile").hover(function () {
+  //   $(".skill_per").css({ "animation-play-state": "running" });
+  // });
+});
